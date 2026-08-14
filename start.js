53f5fc2e51bde7d7e5a4e62e55a9156564af754b
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-15 07:37:56 +0900

    학생 로그인 중 로딩 화면 추가

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 require('./response-safety.js');
 require('./ui-overrides.js');
+require('./student-login-loading.js');
 require('./mobile-growth-nav-fix.js');
 require('./growth-graph.js');
 require('./superadmin.js');

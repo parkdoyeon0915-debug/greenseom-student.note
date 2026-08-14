c814b4767d7812c4d58f84614d00fbfbbd91a5b3
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-15 07:45:09 +0900

    Reset stacked UI patches and keep only stable core fixes

--- start.js
+++ start.js
@@ -1,8 +1,3 @@
 require('./response-safety.js');
-require('./ui-overrides.js');
-require('./student-login-loading.js');
-require('./mobile-growth-nav-fix.js');
-require('./growth-graph.js');
 require('./superadmin.js');
-require('./admin-comment-fix.js');
-require('./admin-comment-final.js');
+require('./stable-core-fix.js');

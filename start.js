b0e69944b8e93247ed4ba9b00d9bda5a5ce25374
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-14 19:48:21 +0900

    Fix student page button JavaScript parsing

--- start.js
+++ start.js
@@ -11,5 +11,18 @@ source=source.replace(
   '["problem_analysis","form_score","completion","expression","composition"]'
 );
 
+// Fix the student-page script typo that commented out the closing brace of savePattern().
+const originalReadFileSync=fs.readFileSync.bind(fs);
+fs.readFileSync=function(file,encoding){
+  const value=originalReadFileSync(file,encoding);
+  if(typeof value==='string' && path.basename(String(file))==='index.html'){
+    return value.replace(
+      "patImages.forEach(f=>fd.append('images',f));// 편집 중에는 새로 선택한 그림만 추가되고 기존 그림은 그대로 유지됩니다.let r=",
+      "patImages.forEach(f=>fd.append('images',f));\n// 편집 중에는 새로 선택한 그림만 추가되고 기존 그림은 그대로 유지됩니다.\nlet r="
+    );
+  }
+  return value;
+};
+
 fs.writeFileSync(runtimePath,source,"utf8");
 require(runtimePath);

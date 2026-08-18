53eabf59350b2af902c71713f27d4c54a81d93e7
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-18 15:54:01 +0900

    Fix problem bank navigation by removing conflicting in-page injection

--- start.js
+++ start.js
@@ -3,5 +3,3 @@ require('./superadmin.js');
 require('./stable-core-fix.js');
 require('./student-login-stability.js');
 require('./ui-nav-fix.js');
-require('./problem-bank-page-fixed.js');
-require('./problem-bank-nav-fix.js');

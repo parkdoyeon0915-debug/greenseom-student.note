af8f404de019368a3324dcf764bbfe6cd4125d34
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-18 15:46:55 +0900

    Route problem bank through existing student page

--- start.js
+++ start.js
@@ -4,3 +4,4 @@ require('./stable-core-fix.js');
 require('./student-login-stability.js');
 require('./ui-nav-fix.js');
 require('./problem-bank-page-fixed.js');
+require('./problem-bank-nav-fix.js');

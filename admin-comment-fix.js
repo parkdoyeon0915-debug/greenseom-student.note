beffe798ca83b09b015550b719b6a1cfb0f00a8c
parkdoyeon0915-debug <parkdoyeon0915@gmail.com>
2026-08-20 01:20:27 +0900

    관리자 코멘트 제목을 선생님들의 코멘트로 통일

--- admin-comment-fix.js
+++ admin-comment-fix.js
@@ -1,2 +1,10 @@
-// 관리자 코멘트 기능은 admin.html에서 단일 구현합니다.
-// 이 파일은 더 이상 서버 시작 시 로드되지 않습니다.
+const express=require('express');
+const originalSend=express.response.send;
+
+// 서버에서 admin.html에 덧붙이는 구버전 관리자 코멘트 제목을 새 명칭으로 통일합니다.
+express.response.send=function(body){
+  if(typeof body==='string'&&this.req&&this.req.path==='/admin.html'){
+    body=body.replaceAll('도연&인혜T의 한마디','선생님들의 코멘트');
+  }
+  return originalSend.call(this,body);
+};
